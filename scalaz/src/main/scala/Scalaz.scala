5a935a3e36dd8be93329f5849437084c885ab3e8
Rob Norris <rob_norris@mac.com>
2014-12-09 12:33:22 -0800

    Catchable[IO]

--- scalaz/src/main/scala/Scalaz.scala
+++ scalaz/src/main/scala/Scalaz.scala
@@ -4,10 +4,17 @@ import matterhorn._
 import IO._
 
 package object matterhorn {
+
   implicit val IOMonad: Monad[IO] = new Monad[IO] {
     def point[A](a: => A): IO[A] = captureIO(a)
     def bind[A, B](fa: IO[A])(f: A => IO[B]): IO[B] = fa.flatMap(f)
     override def map[A, B](fa: IO[A])(f: A => B): IO[B] = fa.map(f)
+  }
+
+  implicit val CatchableMonad: Catchable[IO] = new Catchable[IO] {
+    def attempt[A](f: IO[A]): IO[Throwable \/ A] = 
+      catching[Throwable, Throwable \/ A](f.map(\/-(_)))(e => constIO(-\/(e)))
+    def fail[A](err: Throwable): IO[A] = captureIO(throw err)
   }
 
   implicit val ConcurrentlyApplicative: Applicative[Concurrently] = new Applicative[Concurrently] {
@@ -21,4 +28,5 @@ package object matterhorn {
     def point[A](a: => A): STM[A] = STM(_ => a)
     def bind[A, B](fa: STM[A])(f: A => STM[B]): STM[B] = fa.flatMap(f)
   }
+
 }
